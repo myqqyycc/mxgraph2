55500db4c3779787c9a0b677663d1d49cc35778b
myqqyycc <963785283@qq.com>
2019-06-05 00:21:28 +0800

    初始化

---
+++ src/components/index_view.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="geEditor">dddddddddddddddddddd</div>
+</template>
+
+<script>
+  import {urlParams,mxLoadResources} from '../../static/js/index_view_begin'
+  import Init from '../../static/js/Init'
+  import deflate_pako from '../../static/deflate/pako.min'
+  import {Base64} from '../../static/deflate/base64'
+  import {jscolor} from '../../static/jscolor/jscolor'
+  import sanitizer_sanitizer from '../../static/sanitizer/sanitizer.min'
+  import {mxClient} from '../../static/js/mxClient'
+  import {EditorUi} from '../../static/js/EditorUi'
+
+  import {Editor} from '../../static/js/Editor'
+  import {Sidebar} from '../../static/js/Sidebar'
+  import {Graph} from '../../static/js/Graph'
+  import {Format} from '../../static/js/Format'
+  import {Shapes} from '../../static/js/Shapes'
+  import {Actions} from '../../static/js/Actions'
+  import {Menus} from '../../static/js/Menus'
+  import {Toolbar} from '../../static/js/Toolbar'
+  import {OpenDialog} from '../../static/js/Dialogs'
+
+  $(function () {
+
+  })
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import index_view from '@/components/index_view'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/index_view',
+      name: 'index_view',
+      component: index_view
     }
-  ]
+  ], mode: "history"
 })
